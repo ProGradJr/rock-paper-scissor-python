41c86a54e744156d71c109e662c2f4cec5ce5ca9
ProGrad Junior <70092746+ProGradJr@users.noreply.github.com>
2021-08-31 16:03:12 +0530

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,64 +1,121 @@
-import tkinter as tk
-from tkinter.filedialog import askopenfilename, asksaveasfilename
-import os
-from PIL import Image
-from PIL import ImageTk
-import tkinter.font as TkFont
-import math
-from tkinter.ttk import *
+from tkinter import *
+from PIL import Image, ImageTk
+from random import randint
 
-def open_file():
-    """Open a file for editing."""
-    filepath = askopenfilename(
-        filetypes=[("Reflect File (*.refl)", "*.refl"), ("All Files", "*.*")]
-    )
-    if not filepath:
-        return
-    txt_edit.delete(1.0, tk.END)
-    with open(filepath, "r") as input_file:
-        text = input_file.read()
-        txt_edit.insert(tk.END, text)
-    window.title(f"Reflect Code Editor - {os.path.basename(filepath)}")
+# main window
+root = Tk()
+root.title("Rock Scissor Paper")
+root.configure(background="aqua")
 
-def save_file():
-    """Save the current file as a new file."""
-    filepath = asksaveasfilename(
-        defaultextension=".refl",
-        filetypes=[("Reflect File (*.refl)", "*.refl"), ("All Files", "*.*")],
-    )
-    if not filepath:
-        return
-    with open(filepath, "w") as output_file:
-        text = txt_edit.get(1.0, tk.END)
-        output_file.write(text)
-    window.title(f"Reflect Code Editor - {os.path.basename(filepath)}")
+# picture
+rock_img = ImageTk.PhotoImage(Image.open("./assets/rock-user.png"))
+paper_img = ImageTk.PhotoImage(Image.open("./assets/paper-user.png"))
+scissor_img = ImageTk.PhotoImage(Image.open("./assets/scissors-user.png"))
+rock_img_comp = ImageTk.PhotoImage(Image.open("./assets/rock.png"))
+paper_img_comp = ImageTk.PhotoImage(Image.open("./assets/paper.png"))
+scissor_img_comp = ImageTk.PhotoImage(Image.open("./assets/scissors.png"))
 
-def add_highlighter():
-#    txt_edit.highlight_pattern("word", "red")
-#    txt_edit.tag_add("start", "2.8", "1.13")
-   txt_edit.tag_add("start", "1.11","1.20")
-   txt_edit.tag_config("start", background= "black", foreground= "white")
+# insert picture
+user_label = Label(root, image=scissor_img, bg="aqua")
+comp_label = Label(root, image=scissor_img_comp, bg="aqua")
+comp_label.grid(row=1, column=0)
+user_label.grid(row=1, column=4)
 
-window = tk.Tk()
-window.title("Reflect Code Editor")
-window.rowconfigure(0, minsize=800, weight=1)
-window.columnconfigure(1, minsize=800, weight=1)
-font = TkFont.Font(family="Sans Serif", size=15)
+# scores
+playerScore = Label(root, text=0, font=("Arial", 15), bg="aqua", fg="green")
+computerScore = Label(root, text=0, font=("Arial", 15), bg="aqua", fg="green")
+computerScore.grid(row=1, column=1)
+playerScore.grid(row=1, column=3)
 
-txt_edit = tk.Text(window,font=font)
-txt_edit.tag_configure("start", background="black",foreground='red')
+# indicators
+user_indicator = Label(root, font=("Arial", 15), text="USER", bg="aqua", fg="brown")
+comp_indicator = Label(root, font=("Arial", 15), text="COMPUTER",
+                       bg="aqua", fg="brown")
+user_indicator.grid(row=0, column=3)
+comp_indicator.grid(row=0, column=1)
+
+# messages
+msg = Label(root, font=("Arial", 15), bg="aqua", fg="green")
+msg.grid(row=3, column=2)
+
+# update message
+def updateMessage(message):
+    msg['text'] = message
+
+# update user score
+def updatePlayerScore():
+    score = int(playerScore['text'])
+    score += 1
+    playerScore['text'] = str(score)
+
+# update computer score
+def updateComputerScore():
+    score = int(computerScore['text'])
+    score += 1
+    computerScore['text'] = str(score)
+
+# check winner    
+def checkWin(player,computer):
+    if(player == computer):
+        updateMessage("It's a tie")
+    elif(player == "rock"):
+        if(computer == "scissor"):
+            updateMessage("You Won")
+            updatePlayerScore()
+        else:
+            updateMessage("You Lose")
+            updateComputerScore()
+    elif(player == "paper"):
+        if(computer == "rock"):
+            updateMessage("You Won")
+            updatePlayerScore()
+        else:
+            updateMessage("You Lose")
+            updateComputerScore()
+    elif(player == "scissor"):
+        if(computer == "paper"):
+            updateMessage("You Won")
+            updatePlayerScore()
+        else:
+            updateMessage("You Lose")
+            updateComputerScore()
+    else:
+        pass
+
+# update choices
+
+choices = ["rock", "paper", "scissor"]
 
 
-fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2,bg='black')
-btn_open = tk.Button(fr_buttons, text="Open", command=open_file,bg='white',fg='black',bd=0,font=font)
-btn_save = tk.Button(fr_buttons, text="Save As...", command=save_file,bg='white',fg='black',bd=0,font=font)
-highlight = tk.Button(fr_buttons, text="Highlight", command=add_highlighter,bg='white',fg='black',bd=0,font=font)
+def updateChoice(x):
 
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_save.grid(row=1, column=0, sticky="ew", padx=5)
-highlight.grid(row=2, column=0, sticky="ew", padx=5)
+    # for computer
+    compChoice = choices[randint(0, 2)]
+    if compChoice == "rock":
+        comp_label.configure(image=rock_img_comp)
+    elif compChoice == "paper":
+        comp_label.configure(image=paper_img_comp)
+    else:
+        comp_label.configure(image=scissor_img_comp)
 
-fr_buttons.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky="nsew")
 
-window.mainloop()
+# for user
+    if x == "rock":
+        user_label.configure(image=rock_img)
+    elif x == "paper":
+        user_label.configure(image=paper_img)
+    else:
+        user_label.configure(image=scissor_img)
+
+    checkWin(x, compChoice)
+
+
+# buttons
+rock = Button(root, width=20, height=2, text="ROCK",
+              bg="#FF3E4D", fg="black", font=("Arial", 15), command=lambda: updateChoice("rock")).grid(row=2, column=1)
+paper = Button(root, width=20, height=2, text="PAPER",
+               bg="#FAD02E", fg="black", font=("Arial", 15), command=lambda: updateChoice("paper")).grid(row=2, column=2)
+scissor = Button(root, width=20, height=2, text="SCISSOR",
+                 bg="#0ABDE3", fg="black", font=("Arial", 15), command=lambda: updateChoice("scissor")).grid(row=2, column=3)
+
+root.mainloop()
